9a40835f15145dbbe49618431f37152b7dc52d77
blueblueRice <465320168@qq.com>
2016-10-24 00:46:06 +0800

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
-//gulp
-
 var gulp = require('gulp'),
 	connect = require('gulp-connect'),
-	babel = require('gulp-babel');
+	babel = require('gulp-babel'),
+	webpack = require('webpack-stream');
 
 gulp.task('web-server', function(){
 	connect.server({
@@ -12,21 +11,36 @@ gulp.task('web-server', function(){
 });
 
 gulp.task('babel', function(){
-	return gulp.src('./js/*.js')
+	return gulp.src('./es6/*.js')
 			   .pipe(babel({
 			   		presets: ['es2015']
 			   }))
+			   .pipe(gulp.dest('./es5'))
+    		   .pipe(connect.reload());
+})
+
+gulp.task('webpack', function(){
+	return gulp.src('./es5/*.js')
+			   .pipe(webpack({
+			   		output: {
+			   			filename: '../javascript/boundle.js'
+			   		}
+			   }))
 			   .pipe(gulp.dest('./javascript'))
     		   .pipe(connect.reload());
-
 })
+
+
+gulp.task('index', function(){
+	gulp.src('index.html')
+		.pipe(connect.reload());
+})
+
 
 gulp.task('watch', function(){
 	gulp.watch('./js/*.js', ['babel']);
-	gulp.watch('index.html', function(){
-			gulp.src('index.html')
-				.pipe(connect.reload())
-		})
+	gulp.watch('./es5/*.js', ['webpack']);
+	gulp.watch('index.html', ['index']);
 })
 
-gulp.task('default', ['web-server', 'babel', 'watch']);
+gulp.task('default', ['web-server', 'babel', 'webpack', 'watch']);
